f39da6efe916c7adf15a5f17124ef1e7ef061d92
robin@msctek.com <robin@msctek.com>
2018-02-09 00:13:42 -0600

    removed ref to old project, removed unneeded ref to System.Net.http

--- WeatherWear/WeatherWear.ConsoleApp/Program.cs
+++ WeatherWear/WeatherWear.ConsoleApp/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WeatherWear.ClassLib;
+using WWClassLib;
 
 namespace WeatherWear.ConsoleApp
 {
